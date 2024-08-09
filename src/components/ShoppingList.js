9ede17bca78a37d30ea09dab555c82c46f03c213
slavesword <slaven.vulic@gmail.com>
2024-08-09 14:51:21 +0100

    after holiday break commit

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,30 @@
+import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
+
+function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='jh-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='jh-plant-item'>
+						{plant.name}
+                        {plant.isSpecialOffer && <div className='jh-sales'>Sale</div>}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default ShoppingList
---
+++ src/datas/plantList.js
@@ -0,0 +1,50 @@
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classic',
+		id: '1ed'
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classic',
+		id: '2ab'
+	},
+	{
+		name: 'silver pothos',
+		category: 'classic',
+		id: '3sd',
+		isSpecialOffer: true
+	},
+	{
+		name: 'yucca',
+		category: 'classic',
+		id: '4kk'
+	},
+	{
+		name: 'olive tree',
+		category: 'exterior',
+		id: '5pl'
+	},
+	{
+		name: 'geranium',
+		category: 'exterior',
+		id: '6uo'
+	},
+	{
+		name: 'basil',
+		category: 'exterior',
+		id: '7ie',
+		isSpecialOffer: true
+	},
+	{
+		name: 'aloe',
+		category: 'succulent',
+		id: '8fp',
+		isSpecialOffer: true
+	},
+	{
+		name: 'succulent',
+		category: 'succulent',
+		id: '9vn'
+	}
+]
